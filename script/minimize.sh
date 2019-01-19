544df1be634df20c2bf8b62574380d32fa778e7f
heizo <onihei.kato@gmail.com>
2019-01-19 15:07:09 +0900

    Remove unused code

--- script/minimize.sh
+++ script/minimize.sh
@@ -19,10 +19,6 @@ dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
 #dpkg --list | grep -i compiler | awk '{ print $2 }' | xargs apt-get -y purge
 #apt-get -y purge cpp gcc g++
 #apt-get -y purge build-essential git
-#echo "==> Removing default system Ruby"
-#apt-get -y purge ruby ri doc
-#echo "==> Removing default system Python"
-#apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl libffi5
 echo "==> Removing X11 libraries"
 apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
 echo "==> Removing other oddities"
