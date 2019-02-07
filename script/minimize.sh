bb80b82c01cffdc74fb77c588fefee399add1b50
heizo <onihei.kato@gmail.com>
2019-02-07 20:56:34 +0900

    Clean

--- script/minimize.sh
+++ script/minimize.sh
@@ -25,8 +25,8 @@ dpkg --list | grep '^rc' | awk '{print $2}' | xargs apt-get -y purge
 
 # Clean up orphaned packages with deborphan
 apt-get -y install deborphan
-while [ -n "$(deborphan --guess-all --libdevel)" ]; do
-    deborphan --guess-all --libdevel | xargs apt-get -y purge
+while [ -n "$(deborphan --guess-all)" ]; do
+    deborphan --guess-all | xargs apt-get -y purge
 done
 apt-get -y purge deborphan dialog
 
@@ -35,6 +35,6 @@ apt-get -y autoremove --purge
 apt-get -y clean
 
 echo "==> Removing APT files"
-find /var/lib/apt -type f | xargs rm -f
+find /var/lib/apt -type f -exec rm -rf {} \;
 echo "==> Removing caches"
 find /var/cache -type f -exec rm -rf {} \;
